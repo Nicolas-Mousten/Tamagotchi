bfaef13729b3016862c863e2d0cbc215eef0b40a
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-10-01 14:19:35 +0200

    Pictures added

---
+++ Java/src/Pictures.java
@@ -0,0 +1,61 @@
+public class Pictures {
+    public static void angryDog(){
+        System.out.println("  _.       ._\t\t");
+        System.out.println("  |\\\\___  //|   \t");
+        System.out.println("  | )   `(/ |   |\t");
+        System.out.println("  |/      \\ |  /|\t");
+        System.out.println("  / _   _  `|_ ||\t");
+        System.out.println("  | O)  O   ) \\||\t");
+        System.out.println("  \\        /  | |\t");
+        System.out.println(" / \\ Y   /'   | \\\t");
+        System.out.println(" |  `._/' |   |  \\\t");
+        System.out.println(" |  |`-._/   /    \\\t");
+        System.out.println(" \\  \\   /   / \\_.  \\\t");
+        System.out.println("  \\  \\ |   /    |  |\t");
+        System.out.println("   \\uU \\UU/     |  /\t");
+    }
+    public static void normalDog(){
+        System.out.println("            \t          _");
+        System.out.println("                       ,:'/   _..._");
+        System.out.println("                      // ( `\"\"-.._.'");
+        System.out.println("                      \\| /    6\\___");
+        System.out.println("                      |     6        4");
+        System.out.println("                      |              /");
+        System.out.println("                      \\_       .--'");
+        System.out.println("                       (_'---'`)");
+        System.out.println("                      / `'---`()");
+        System.out.println("                    ,'        |");
+        System.out.println("    ,            .'`          |");
+        System.out.println("    )\\       _.-'             ;");
+        System.out.println("   / |    .'`   _            /");
+        System.out.println(" /` /   .'       '.        , |");
+        System.out.println("/  /   /           \\   ;   | |");
+        System.out.println("|  \\  |            |  .|   | |");
+        System.out.println(" \\  `\"|           /.-' |   | |");
+        System.out.println("  '-..-\\       _.;.._  |   |.;-.");
+        System.out.println("        \\    <`.._  )) |  .;-. ))");
+        System.out.println("        (__.  `  ))-'  \\_    ))'");
+        System.out.println("            `'--\"`         `\"\"\"`");
+    }
+    public static void unStimulatedCat(){
+        System.out.println("(\"`-''-/\").___..--''\"`-._ ");
+        System.out.println(" `6_ 6  )   `-.  (     ).`-.__.`) ");
+        System.out.println(" (_Y_.)'  ._   )  `._ `. ``-..-' ");
+        System.out.println("   _..`--'_..-_/  /--'_.'");
+        System.out.println("  ((((.-''  ((((.'  (((.-'");
+    }
+    public static void tiredCat(){
+        System.out.println("          __..--''``---....___   _..._    __");
+        System.out.println(" /// //_.-'    .-/\";  `        ``<._  ``.''_ `. / // /");
+        System.out.println("///_.-' _..--.'_    \\                    `( ) ) // //");
+        System.out.println("/ (_..-' // (< _     ;_..__               ; `' / ///");
+        System.out.println(" / // // //  `-._,_)' // / ``--...____..-' /// / //");
+    }
+    public static void normalCat(){
+        System.out.println("    /\\_/\\           ___");
+        System.out.println("   = o_o =_______    \\ \\ ");
+        System.out.println("    __^      __(  \\.__) )");
+        System.out.println("(@)<_____>__(_____)____/");
+
+    }
+}
